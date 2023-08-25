2ee05e761f5e5d347af502ac521b9955a440e056
Nathan Bleuzen <nathan.bleuzen@ledger.fr>
2023-08-25 09:46:18 +0200

    feat: add monitor script to relaunch ffmpeg if the rtsp stream hangs

---
+++ ffmpeg_monitor.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+ffmpeg_script_pid=0
+readonly FFMPEG_SCRIPT="./ffmpeg_rtsp_recorder.sh" # Path to the script using ffmpeg
+
+kill-ffmpeg() {
+    # Forcefully terminate ffmpeg process
+    ffmpeg_pid=$(pgrep --parent ${ffmpeg_script_pid})
+    kill -9 ${ffmpeg_pid}
+
+    kill -9 ${ffmpeg_script_pid}
+}
+
+launch-ffmpeg() {
+    # Start the ffmpeg recording script in the background
+    bash "${FFMPEG_SCRIPT}" &
+
+    # Get the PID of the most recently started ffmpeg process
+    ffmpeg_script_pid=$!
+}
+
+# Function to handle Ctrl+C (SIGINT) signal
+function graceful_exit {
+    echo "Terminating the monitor script..."
+    kill-ffmpeg
+    exit 0
+}
+
+# Set up signal handler for SIGINT
+trap graceful_exit SIGINT
+
+readonly CPU_THRESHOLD_PERCENT=2
+readonly MONITOR_PERIOD_SEC=300 # Period at which the ffmpeg script will be monitored
+readonly DEBOUNCE_COUNT=10
+readonly DEBOUNCE_PERIOD_SEC=3
+
+# Initially start ffmpeg
+launch-ffmpeg
+
+while true; do
+    # Wait for a while before checking CPU utilization
+    sleep ${MONITOR_PERIOD_SEC}
+
+    below_threshold_count=0
+    for ((i=1; i<=DEBOUNCE_COUNT; i++)); do
+        # Wait for a while before checking CPU utilization
+        sleep ${DEBOUNCE_PERIOD_SEC}
+
+        # Get CPU utilization of ffmpeg process
+        ffmpeg_pid=$(pgrep --parent ${ffmpeg_script_pid})
+        cpu_utilization=$(top -b -n 1 -p ${ffmpeg_pid} | grep "${ffmpeg_pid}" | awk '{print $9}' | tr ',' '.')
+
+        if [ "$(echo "$cpu_utilization < $CPU_THRESHOLD_PERCENT" | bc)" -eq 1 ]; then
+            ((below_threshold_count++))
+        fi
+    done
+
+    if [ "$below_threshold_count" -eq "$DEBOUNCE_COUNT" ]; then
+        # Restart ffmpeg
+        kill-ffmpeg
+        launch-ffmpeg
+    fi
+done
--- ffmpeg_rtsp_recorder.sh
+++ ffmpeg_rtsp_recorder.sh
@@ -1,37 +1,36 @@
 #!/bin/bash
-set -x
 
 # Function to handle Ctrl+C (SIGINT) signal
 function graceful_exit {
-    echo "Terminating the script..."
+    echo "Terminating the ffmpeg script..."
     exit 0
 }
 
 # Set up signal handler for SIGINT
 trap graceful_exit SIGINT
 
-RTSP_URL="rtsp://securehome:testingMyCamera@192.168.1.38:554/stream1"
+# To complete with your informations
+RTSP_URL="rtsp://username:password@192.168.1.10:554/stream1"
 OUTPUT_FILENAME="securehome_"
-DURATION_SEC=10 # Duration of each recording in seconds
+DURATION_SEC=900 # Duration of each recording in seconds
 MAX_RECORDINGS=2 # +1 recording files will be kept
 
 while true; do
     # Delete older recordings if there are more than MAX_RECORDINGS
-    recordings_count=$(ls -1 "${OUTPUT_FILENAME}"* | wc -l)
-    echo "${recordings_count}"
-    if [ "${recordings_count}" -gt "${MAX_RECORDINGS}" ]; then
+    while [ "$(ls -1 "${OUTPUT_FILENAME}"* | wc -l)" -gt "${MAX_RECORDINGS}" ]; do
         oldest_recording=$(ls -t "${OUTPUT_FILENAME}"* | tail -n 1)
         rm "${oldest_recording}"
-    fi
+    done
 
     TIMESTAMP=$(date +'%Y-%m-%d_%H:%M:%S')
-    # Record the stream
-    # -stimeout to disconnect after that many micro seconds if there is a network issue during connection the RTSP stream (here 10 sec)
+    # Record the stream -stimeout to disconnect after that many micro seconds if
+    # there is a network issue during connection the RTSP stream (here 10 sec)
     ffmpeg \
         -rtsp_transport tcp \
         -i "${RTSP_URL}" \
         -stimeout 10000000 \
         -t "${DURATION_SEC}" \
         -c:v copy \
-        -y "${OUTPUT_FILENAME}${TIMESTAMP}.mp4"
+        -y "${OUTPUT_FILENAME}${TIMESTAMP}.mp4" \
+        > /dev/null 2>&1
 done
